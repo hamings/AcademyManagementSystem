8f7f52ce51030cdca8575cf8e00af5477dc48685
hamings <wjdgpal9630@gmail.com>
2024-03-25 08:53:38 +0900

    #16 [feat] 로그인, 로그아웃 기능 구현

--- src/src/service/UserService.java
+++ src/src/service/UserService.java
@@ -1,4 +1,49 @@
 package src.service;
 
+import src.ServiceType;
+import src.domain.Admin;
+import src.domain.Student;
+import src.domain.Teacher;
+import src.repository.Repository;
+import src.repository.RepositoryProvider;
+
+import java.io.IOException;
+
 public class UserService {
+    private Repository<Student,String> studentRepository;
+    private Repository<Teacher,String> teacherRepository;
+    private Admin admin;
+
+    public UserService() throws IOException {
+        this.studentRepository = RepositoryProvider.getInstance().provide(ServiceType.STUDENT);
+        this.teacherRepository = RepositoryProvider.getInstance().provide(ServiceType.TEACHER);
+        this.admin = Admin.getInstance();
+    }
+    public Student loginStudent(String id, String pw) throws IOException {
+
+        Student student = studentRepository.findById(id);
+        if(student!=null && student.getPassword().equals(pw)){
+            return student;
+        }
+        return null;
+
+    }
+    public Teacher loginTeacher(String id, String pw) throws IOException {
+        Teacher teacher = teacherRepository.findById(id);
+        if(teacher!=null && teacher.getPassword().equals(pw)){
+            return teacher;
+        }
+        return null;
+
+    }
+    public Admin loginAdmin(String id, String pw){
+        if(admin.getId().equals(id)&&admin.getPassword().equals(pw)){
+            return admin;
+        }
+        return null;
+    }
+    public Object logout(){
+        return null;
+    }
+
 }
