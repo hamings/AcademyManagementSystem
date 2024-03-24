f874b9c9000e4972aceb568bd559cd1fb0bbab5c
hamings <wjdgpal9630@gmail.com>
2024-03-24 16:57:01 +0900

    #3 [feat] 강사의 담당강의 리스트 출력 기능 구현

--- src/src/service/TeacherService.java
+++ src/src/service/TeacherService.java
@@ -1,6 +1,35 @@
 package src.service;
 
+import src.ServiceType;
+import src.domain.Lecture;
+import src.domain.LectureRegistration;
+import src.domain.Student;
+import src.domain.Teacher;
+import src.repository.Repository;
+import src.repository.RepositoryProvider;
+
+import java.io.IOException;
 
 public class TeacherService {
+    private Teacher teacher;
+    private Repository<Student,String> studentRepository;
+
+
+    public TeacherService() throws IOException {
+        this.studentRepository = RepositoryProvider.getInstance().provide(ServiceType.STUDENT);
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    /**
+     * 강사의 강의 리스트를 보여주는 함수
+     */
+    public void showLectureList(){
+        for(Lecture lecture: teacher.getLectureList()){
+            System.out.println(lecture);
+        }
+    }
 
 }
