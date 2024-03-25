51f2b2fbf4c673a9df4a0636df485799f5765b96
hamings <wjdgpal9630@gmail.com>
2024-03-25 17:20:31 +0900

    #28 [feat] main 메서드 작성

---
+++ src/src/util/Constant.java
@@ -0,0 +1,17 @@
+package src.util;
+
+public class Constant {
+    public static final String STUDENT_MENU = "1.수강관리 2.자습실관리 3.알림확인 4.로그아웃";
+    public static final String TEACHER_MENU = "1.강의 리스트, 2. 강의별 학생 리스트 3.로그아웃";
+    public static final String ADMIN_MENU = "1.학생관리 2.강사관리 3.강의관리 4.납부관리 5.로그아웃";
+    public static final String ADMIN_STUDENTSYSTEM_MENU = "1.학생상세 2.학생수정 3.학생삭제";
+    public static final String ADMIN_TEACHERSYSTEM_MENU = "1.강의상세 2.강의등록 3.강의삭제";
+    public static final String ADMIN_LECTURESYSTEM_MENU = "1.강의상세 2.강의등록 3.강의삭제";
+    public static final String INPUT_LECTURE_ID = "강의ID를 입력해주세요";
+    public static final String INPUT_STUDENT_ID = "학생ID를 입력해주세요";
+    public static final String INPUT_TEACHER_ID = "강사ID를 입력해주세요";
+    public static final String INPUT_NUMBER = "번호를 입력해주세요";
+    public static final String NO_MATCH_NUMBER = "번호가 잘못 입력되었습니다.";
+
+
+}
