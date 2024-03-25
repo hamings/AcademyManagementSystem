02713480a6d353472070084f6fdde2d19fbd015a
hamings <wjdgpal9630@gmail.com>
2024-03-25 17:14:15 +0900

    #28 [feat] admin 싱글톤 구현

--- src/src/domain/Admin.java
+++ src/src/domain/Admin.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class Admin {
+    private static Admin admin = new Admin();
     private String id;
     private String password;
 
@@ -11,9 +12,12 @@ public class Admin {
         this.id = "admin";
         this.password = "admin123";
     }
-    //TODO 내용 수정 필요
+
     public static Admin getInstance(){
-        return new Admin();
+        if(admin==null){
+            admin = new Admin();
+        }
+        return admin;
     }
 
 }
