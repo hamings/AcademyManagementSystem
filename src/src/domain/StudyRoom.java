cd13d9c54532636c242ed977bb3af56fee5fe2b6
hamings <wjdgpal9630@gmail.com>
2024-03-25 17:16:08 +0900

    #28 [feat] 초기화 추가

--- src/src/domain/StudyRoom.java
+++ src/src/domain/StudyRoom.java
@@ -2,6 +2,7 @@ package src.domain;
 
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
@@ -10,6 +11,12 @@ public class StudyRoom {
     private static final int ROW = 5; // 임의 할당 -0> 추후 수정
     private static final int COL = 5;
 
-    private boolean[][] checkSeat;
-    private Map<String, String> reservationMap;
+    private boolean[][] checkSeat = {
+            {false, false, false, false, false},
+            {false, false, false, false, false},
+            {false, false, false, false, false},
+            {false, false, false, false, false},
+            {false, false, false, false, false}
+    };
+    private Map<String, String> reservationMap = new HashMap<>();
 }
